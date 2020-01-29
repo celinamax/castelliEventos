63f9d5502c2b7dd99c3d919931df8d0ff47574fa
Celina Maximiano <celinamax01@gmail.com>
2020-01-29 14:41:31 -0300

    Tela cadastro saida

---
+++ src/Controller/CadastroSaidaController.java
@@ -0,0 +1,18 @@
+
+package Controller;
+
+import view.CadastroSaida;
+
+public class CadastroSaidaController {
+    
+    private final CadastroSaida view;
+    
+    public CadastroSaidaController(CadastroSaida view){
+        this.view = view;        
+    }
+    
+//    public Cadastro obterDadosConvidado(){
+//        String digital = view.getjTextFieldDigital().getText();
+//    }
+//    
+}
