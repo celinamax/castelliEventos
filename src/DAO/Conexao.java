ae10fa4220df4d6e5bda913d6d54b2e72108034b
luisfelipe <luisfelipedesenvhms@gmail.com>
2020-02-05 09:30:09 -0300

    teste

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -1,8 +1,4 @@
-
-
 package DAO;
-
-
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,28 +10,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Base64;
 
-
-
 public class Conexao {
-    
+
     public Connection getConnection() throws SQLException, FileNotFoundException, IOException {
-        
+
         File file = new File("C:\\Castelli\\configuracao.txt");
         if (file.exists()) {
             FileReader fileR = new FileReader(file);
             BufferedReader buffR = new BufferedReader(fileR);
             String caminho_banco = buffR.readLine();
             String usuario = buffR.readLine();
-            String senha = buffR.readLine(); 
-            
+            String senha = buffR.readLine();
+
             caminho_banco = new String(Base64.getDecoder().decode(caminho_banco.getBytes()));
             usuario = new String(Base64.getDecoder().decode(usuario.getBytes()));
             senha = new String(Base64.getDecoder().decode(senha.getBytes()));
-            
+
             Connection conexao = DriverManager.getConnection(caminho_banco, usuario, senha);
             return conexao;
-        }else{
-           return null; 
-        } 
+        } else {
+            return null;
+        }
     }
 }
