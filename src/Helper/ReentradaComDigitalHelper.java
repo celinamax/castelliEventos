4a56dcf72679ea14791e64145d1733220a98e48b
Celina Maximiano <celinamax01@gmail.com>
2020-02-06 14:39:59 -0300

    Teste

--- src/Helper/ReentradaComDigitalHelper.java
+++ src/Helper/ReentradaComDigitalHelper.java
@@ -1,22 +1,18 @@
-
 package Helper;
 
 import view.ReentradaView;
 
 public class ReentradaComDigitalHelper {
-    
+
     private final ReentradaView view;
 
     public ReentradaComDigitalHelper(ReentradaView view) {
         this.view = view;
     }
 
-    
     public String comDigital() {
         return view.getjLabelDigital().getText();
-        
+
     }
 
-   
-    
 }
